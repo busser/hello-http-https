c868f240d6c0b03b796ae4b95f3a224808fca6f4
Arthur Busser <git@busser.me>
2018-08-28 14:55:13 +0200

    make server configurable

--- README.md
+++ README.md
@@ -28,5 +28,43 @@ openssl req \
 ## Start the server
 
 ```bash
-hello-http-https
+./hello-http-https
+```
+
+### Server configuration
+
+The server is configurable through CLI flags. For details on usage, run the
+following command:
+
+```bash
+./hello-http-https -help
+```
+
+You should see something like this:
+
+```
+Usage of ./hello-http-https:
+  -help
+    	Prints this usage message
+  -http-port int
+    	Port used to listen for HTTP requests (default 80)
+  -https-port int
+    	Port used to listen for HTTPS requests (default 443)
+  -ssl-certificate string
+    	SSL certificate file for HTTPS server (default "server.crt")
+  -ssl-key string
+    	SSL key file for HTTPS server (default "server.key")
+  -uri-path string
+    	URI path to respond to (default "/hello-world")
+```
+
+The server's default behavior is to listen for HTTP requests on port 80 and for
+HTTPS requests on port 443. It will respond to requests to `/hello-world` with a
+short message, and a 404 error code for other paths.
+
+If you wish for the server to answer to all paths, use the `-uri-path` flag with
+the `/` value:
+
+```bash
+./hello-http-https -uri-path /
 ```
--- main.go
+++ main.go
@@ -1,36 +1,89 @@
 package main
 
 import (
-  "log"
-  "net/http"
-  "sync"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"sync"
 )
 
+// HelloHandler writes a short message to w.
+func HelloHandler(w http.ResponseWriter, req *http.Request) {
+	w.Write([]byte("Hello, World!\n"))
+}
+
+// HelloConfiguration contains any necessary configuration.
+type HelloConfiguration struct {
+	httpPort          int
+	httpsPort         int
+	serverKey         string
+	serverCertificate string
+	uriPath           string
+}
+
+// ParseFlags parses CLI flags for any configuration parameters.
+func ParseFlags() HelloConfiguration {
+  var conf HelloConfiguration
+	var helpWanted bool
+
+	flag.BoolVar(&helpWanted, "help", false, "Prints this usage message")
+  flag.IntVar(&conf.httpPort, "http-port", 80, "Port used to listen for HTTP requests")
+  flag.IntVar(&conf.httpsPort, "https-port", 443, "Port used to listen for HTTPS requests")
+  flag.StringVar(&conf.serverKey, "ssl-key", "server.key", "SSL key file for HTTPS server")
+  flag.StringVar(&conf.serverCertificate, "ssl-certificate", "server.crt", "SSL certificate file for HTTPS server")
+  flag.StringVar(&conf.uriPath, "uri-path", "/hello-world", "URI path to respond to")
+
+	flag.Parse()
+
+	if helpWanted {
+		fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
+		flag.PrintDefaults()
+		os.Exit(0) // Exit after printing usage
+	}
+
+  return conf
+}
+
+// ListenHttpHttps starts HTTP and HTTPS servers based on conf.
+func ListenHttpHttps(conf HelloConfiguration) {
+  // wg is used to stop the function from returning right after starting the
+  // HTTP and HTTPS servers.
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	http.HandleFunc(conf.uriPath, HelloHandler)
+
+  // Start the HTTP server.
+	go func() {
+		defer wg.Done()
+    fmt.Println("Starting to listen for HTTP requests on port", conf.httpPort)
+		err := http.ListenAndServe(fmt.Sprint(":", conf.httpPort), nil)
+		if err != nil {
+			log.Fatal(fmt.Sprint("Could not listen on port ", conf.httpPort, ": "), err)
+		}
+	}()
+
+  // Start the HTTPS server.
+	go func() {
+		defer wg.Done()
+    fmt.Println("Starting to listen for HTTPS requests on port", conf.httpsPort)
+		err := http.ListenAndServeTLS(fmt.Sprint(":", conf.httpsPort), conf.serverCertificate, conf.serverKey, nil)
+		if err != nil {
+			log.Fatal(fmt.Sprint("Could not listen on port ", conf.httpsPort, ": "), err)
+		}
+	}()
+
+	wg.Wait()
+}
+
 func main() {
-  var wg sync.WaitGroup
-  wg.Add(2)
-
-  helloHandler := func(w http.ResponseWriter, req *http.Request) {
-    w.Write([]byte("Hello, World!\n"))
-  }
-
-  http.HandleFunc("/hello-world", helloHandler)
-
-  go func() {
-    defer wg.Done()
-    err := http.ListenAndServe(":80", nil)
-    if err != nil {
-      log.Fatal("Could not listen on port 80: ", err)
-    }
-  }()
-
-  go func() {
-    defer wg.Done()
-    err := http.ListenAndServeTLS(":443", "server.crt", "server.key", nil)
-    if err != nil {
-      log.Fatal("Could not listen on port 443: ", err)
-    }
-  }()
-
-  wg.Wait()
+	// conf := HelloConfiguration{
+	// 	httpPort: 80, httpsPort: 443,
+	// 	serverKey: "server.key", serverCertificate: "server.crt",
+	// 	uriPath: "/hello-world",
+	// }
+  conf := ParseFlags()
+	ListenHttpHttps(conf)
 }
